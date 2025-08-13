5798a72af93598612cd14e98d5e80b367122c37a
CCesar-c <lianbastiam@gmail.com>
2025-08-13 17:10:19 -0300

    create

--- jogo/game.js
+++ jogo/game.js
@@ -3,13 +3,11 @@ const { Engine, Runner, Bodies, World, Body, Events } = Matter;
 const canvas = document.getElementById("game");
 const ctx = canvas.getContext("2d");
 
+const playerImage = new Image();
+playerImage.src = "images.jpg";
+
 const engine = Engine.create();
 const world = engine.world;
-
-// jugador
-var force = 3;
-const player = Bodies.rectangle(400, 300, 50, 50);
-World.add(world, [player]);
 
 // Iniciar motor
 const runner = Runner.create();
@@ -18,15 +16,20 @@ Runner.run(runner, engine);
 engine.world.gravity.y = 0;
 engine.world.gravity.x = 0;
 
+// jugador
+var force = 3;
+const player = Bodies.rectangle(1280 / 2, 720 / 2, 100, 100);
+World.add(world, [player]);
+
 // Movimiento horizontal
 const keys = {};
 document.addEventListener("keydown", (e) => keys[e.key] = true);
 document.addEventListener("keyup", (e) => keys[e.key] = false);
 
 Events.on(engine, "beforeUpdate", () => {
-    if (keys["a"] || keys["ArrowUp"]) Body.setPosition(player, { x: player.position.x - force, y: player.position.y });
+    if (keys["a"] || keys["ArrowLeft"]) Body.setPosition(player, { x: player.position.x - force, y: player.position.y });
     if (keys["d"] || keys["ArrowRight"]) Body.setPosition(player, { x: player.position.x + force, y: player.position.y });
-    if (keys["w"] || keys["ArrowLeft"]) Body.setPosition(player, { x: player.position.x, y: player.position.y - force });
+    if (keys["w"] || keys["ArrowUp"]) Body.setPosition(player, { x: player.position.x, y: player.position.y - force });
     if (keys["s"] || keys["ArrowDown"]) Body.setPosition(player, { x: player.position.x, y: player.position.y + force });
 });
 
@@ -34,11 +37,7 @@ Events.on(engine, "beforeUpdate", () => {
 (function draw() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
 
-    ctx.fillStyle = "black";
-    ctx.fillRect(player.position.x - 0, player.position.y - 25, 50, 50);
-
-    ctx.fillStyle = "black";
-    ctx.fillRect(player.position.x - 0, player.position.y - 25, 50, 50);
+    ctx.drawImage(playerImage, player.position.x -50, player.position.y -50, 100, 100);
 
     requestAnimationFrame(draw);
 })();
