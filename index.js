aaad91429858fd9b3185d6884ded2fbcfe8790ca
CCesar-c <lianbastiam@gmail.com>
2025-08-14 14:45:05 -0300

    button

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
-document.querySelector(".Comecar").addEventListener("click", () => {
-    window.open("jogo/games.html")
+var pop = document.querySelector("pop")
+document.querySelector(".open").addEventListener("click", () => {
+    pop.classList.remove("desactive")
+    pop.classList.add("active");
 })
