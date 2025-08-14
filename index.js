ac3160dcbfef8234d283b4b982a23850fd771e7a
Davi Grah <davigrah2010@gmail.com>
2025-08-14 14:47:47 -0300

    Create button opções

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
 document.querySelector(".Comecar").addEventListener("click", () => {
     window.open("jogo/games.html")
 })
+document.querySelector(".Opções").addEventListener("click", () => {
+    window.open("jogo/Opções.html")
+})
